ce043a6c48e8ba2938208d831ef6268dd59ed599
ltubbs <larry.tubbs@gmail.com>
2018-09-04 08:55:28 -0500

    fixed the code to use the url passed in the event, saftyed for null

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,7 +11,11 @@ export class SidebarComponent implements OnInit {
   currentUrl: string;
 
   constructor(private router: Router) {
-    router.events.subscribe((_: NavigationEnd) => this.currentUrl = this.router.url);
+    router.events.subscribe((_: NavigationEnd) => {
+      if (!!_.url) {
+        this.currentUrl = _.url;
+      }
+    });
    }
 
   ngOnInit() {
